41f31b0e58526f5fa9e4151d2be82ccb3c285934
Christopher Roy <christopher.roy@mail.mcgill.ca>
2018-02-03 16:37:30 -0500

    Added LineListeners, play pause functionality

--- src/mchacks2018/AudioPlayer.java
+++ src/mchacks2018/AudioPlayer.java
@@ -1,15 +1,28 @@
 package mchacks2018;
 import javax.sound.sampled.*;
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
-
-public class AudioPlayer {
+public class AudioPlayer implements LineListener {
 
 	private File soundFile;
 	private long runtime;
+	private boolean playing = false;
 	private Clip playingClip;
 	
 	public static void main(String [] args) {
+		String filename = "src/mchacks2018/sounds/test.wav";
+		AudioPlayer player = new AudioPlayer(filename);
+		player.playAudio();
+		player.wait(5);
+		player.pauseAudio();	
+		player.wait(2);
+		player.resumeAudio();
+		player.wait(2);
+		player.pauseAudio();
+		player.wait(1);
+		player.resumeAudio();
+		player.wait(10);
 		
 	}
 	public AudioPlayer(String filename) {
@@ -17,9 +30,9 @@ public class AudioPlayer {
 			soundFile = new File(filename);			
 			playingClip = AudioSystem.getClip();
 			playingClip.open(AudioSystem.getAudioInputStream(soundFile));
-						
+			playingClip.addLineListener(this);			
 			runtime = playingClip.getMicrosecondLength()/1000;
-			Thread.sleep(runtime);
+			
 		} catch (Exception e) {
 			
 		}
@@ -27,19 +40,23 @@ public class AudioPlayer {
 		
 	}
 	public void playAudio() {
+		playingClip.addLineListener(this);
 		playingClip.start();
+		playing = true;
 	}
 	
 	
 	public void pauseAudio() {
 		playingClip.stop();
+		playing = false;
 	}
 	
-	public void resumeAudio() {
-		if(playingClip.isOpen() && !playingClip.isActive()) {
+	public void resumeAudio() {	
+			playingClip.addLineListener(this);
 			playingClip.start();
-		}
+			playing = true;
 	}
+		
 	
 	public void stopAudio() {
 		playingClip.stop();
@@ -53,6 +70,28 @@ public class AudioPlayer {
 	
 	public void changePosition(long slidervalue) {
 		playingClip.setMicrosecondPosition(slidervalue);
+		
+	}
+	
+	private void wait(int seconds) {
+		try {
+			TimeUnit.SECONDS.sleep(seconds);
+			
+			}
+			catch(Exception e) {
+				
+			}
+	}
+	@Override
+	public void update(LineEvent event) {
+		LineEvent.Type type = event.getType();
+		
+		if(type == LineEvent.Type.START) {
+			System.out.println("Playback Started");
+		} else if(type == LineEvent.Type.STOP) {
+			playing = false;
+			System.out.println("Playback Paused");
+		}
 		
 	}
 	
