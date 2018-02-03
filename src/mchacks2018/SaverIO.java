cc8bd6bf855864a71fcaff83aa642183798ab5e7
adamwer <adam.wertheimer@mail.mcgill.ca>
2018-02-03 15:37:23 -0500

    finish getter/setter methods and added example Recording object

--- src/mchacks2018/SaverIO.java
+++ src/mchacks2018/SaverIO.java
@@ -13,34 +13,78 @@ class Recording implements Serializable{
 		this.time = time;					// Start time in form hhmmT where T is either AM/PM (boolean?) 
 	}
 	
-	// Getters and Setters:
-	public String getSubject(Recording r) {
-		return r.subject;
+	// Getter Methods:
+	public String getTitle() {
+		return this.getSubject() + " (" + this.getDate() + "): " + this.getDescription();
+		// DO THIS - returns Subject (date, time): Description
 	}
-	public String getDescription(Recording r) {
-		return r.description;
+	public String getSubject() {
+		return this.subject;
 	}
-	public String getDate(Recording r) {
-		return r.date;
+	public String getDescription() {
+		return this.description;
 	}
-	public String getMonth(Recording r) {
-		return r.date.substring(0,1);
+	public String getDateString() {
+		return this.date;
 	}
-	public String getDay(Recording r) {
-		return r.date.substring(2,3);
+	public int getMonthNumber() {
+		return Integer.parseInt(this.date.substring(0,2));
+	}
+	public String getMonth() {
+		int monthNumber = this.getMonthNumber();
+		String monthString;
+		switch(monthNumber) {
+		case 1: monthString = "January"; break;
+		case 2: monthString = "February"; break;
+		case 3: monthString = "March"; break;
+		case 4: monthString = "April"; break;
+		case 5: monthString = "May"; break;
+		case 6: monthString = "June"; break;
+		case 7: monthString = "July"; break;
+		case 8: monthString = "August"; break;
+		case 9: monthString = "September"; break;
+		case 10: monthString = "October"; break;
+		case 11: monthString = "November"; break;
+		case 12: monthString = "December"; break;
+		default: monthString = "Invalid Month"; break;
+		}
+		return monthString;
+	}
+	public int getDayNumber() {
+		return Integer.parseInt(this.date.substring(2,4));
 	}	
-	public String getYear(Recording r) {
-		return "20" + r.date.substring(4,5);
+	public String getDay() {
+		int dayNumber = this.getDayNumber();
+		String prefix;
+		switch(dayNumber) {
+		case 1: prefix = "st"; break;
+		case 2: prefix = "nd"; break;
+		case 3: prefix = "rd"; break;
+		case 21: prefix = "st"; break;
+		case 22: prefix = "nd"; break;
+		case 23: prefix = "rd"; break;
+		case 31: prefix = "st"; break;
+		default: prefix = "th"; break;
+		}
+		return dayNumber + prefix;
 	}
-	public String getTime(Recording r) {
-		return r.time;
+	public int getYear() {
+		return 2000 + Integer.parseInt(this.date.substring(4,6));
 	}
-	public String getHour(Recording r) {
-		return r.time.substring(0,1);
+	public String getDate() {
+		return this.getMonth() + " " + this.getDay() + ", " + this.getYear();
 	}
-	public String getMinute(Recording r) {
-		return r.time.substring(2,3);
+	public String getTime() {
+		return this.time;
 	}
+	public String getHour() {
+		return this.time.substring(0,1);
+	}
+	public String getMinute() {
+		return this.time.substring(2,3);
+	}	
+	
+	// Setter Methods:
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
@@ -55,21 +99,25 @@ class Recording implements Serializable{
 	}
 	
 	
-	
-
 	@Override
 	public String toString() {
-		return this.subject + " at " + date + time;
+		return this.getTitle();
 	}
 }
 
 
-// Class will serialize(save) objects as binary files.
+// SaverIO Class will serialize(save) objects as binary files.
 public class SaverIO {
 	
 	// Main Method:
 	public static void main(String[] args) {
-		Recording myRecording = new Recording("MATH-323", "First Probability Lecture", "012518", "08300");
+		Recording myRecording = new Recording("MATH-323", "First Probability Lecture", "010318", "08300");
+		try {
+			saveToFile(myRecording);
+		}
+		catch (IOException e) {
+			e.getMessage();
+		}
 	}
 	
 	public static void saveToFile(Recording r) throws IOException {
