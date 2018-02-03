2885de3a8a4868ff6f2260408f1796b5f942e793
adamwer <adam.wertheimer@mail.mcgill.ca>
2018-02-03 13:05:17 -0500

    added factorial method cause why not

--- src/mchacks2018/Main.java
+++ src/mchacks2018/Main.java
@@ -4,5 +4,17 @@ public class Main {
 	public static void main (String[] args) {
 	System.out.println("Hello World!!");
 	System.out.println("Hello");
-}
+	int n = factorial(5);
+	System.out.println(n);
+	}
+	
+	public static int factorial(int n) {
+		if(n < 0) {
+			return 0;
+		}
+		else if(n == 0 || n == 1) {
+			return 1;
+		}
+		else return n*factorial(n-1);
+	}
 }
