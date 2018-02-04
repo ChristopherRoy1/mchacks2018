a2bbb440e4830664f40ab6ce6a9e0413ccb1e2f4
Christopher Roy <christopher.roy@mail.mcgill.ca>
2018-02-03 21:14:38 -0500

    Got system mixer info in AudioRecorder class

--- src/mchacks2018/AudioRecorder.java
+++ src/mchacks2018/AudioRecorder.java
@@ -1,12 +1,16 @@
 package mchacks2018;
 
-
+import javax.sound.sampled.*;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 public class AudioRecorder {
-
-	
-	
-	
-	
+		
+	public static void main(String [] args) {
+		for( Mixer.Info e : AudioSystem.getMixerInfo()) {
+			System.out.println("Mixer name: " + e.getName());
+			System.out.println("Description: " + e.getDescription());
+		}
+	}
 	
 }
